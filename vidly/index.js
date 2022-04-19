5fd29b4ab8ba152b2653ad73247fa3308eeda3b9
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-20 00:06:59 +0530

    course post and put method added with joi validation

--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const Joi = require("joi");
 const res = require("express/lib/response");
 
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "MERN" },
@@ -20,12 +23,53 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) {
-    res.status(404).send("Course not found");
+    return res.status(404).send("Course not found");
   }
   res.send(course);
 });
 
-const port = process.env.PORT || 3000;
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    return res.status(400).send(result.error.message);
+  }
+
+  console.log("result", result);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send("Course not found");
+  }
+
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).required(),
+  });
+
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    return res.status(400).send(result.error.message);
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+const port = process.env.PORT || 3100;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
