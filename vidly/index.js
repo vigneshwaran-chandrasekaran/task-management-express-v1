8913f03625a98c30966036c4f3f413bea05564e5
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-19 23:08:11 +0530

    courses get, get by id done

--- vidly/index.js
+++ vidly/index.js
@@ -3,20 +3,26 @@ const res = require("express/lib/response");
 
 const app = express();
 
+const courses = [
+  { id: 1, name: "MERN" },
+  { id: 2, name: "React Native" },
+  { id: 3, name: "React" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
-});
-
-app.get("/api/courses/:year/:month", (req, res) => {
-  res.send([req.params, req.query]);
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("Course not found");
+  }
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
