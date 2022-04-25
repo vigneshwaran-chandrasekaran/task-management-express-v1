656c7d3c4f5b02f8690313984899422a946ac999
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-26 00:43:15 +0530

    update single genre done

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -65,19 +65,23 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre) {
-    return res.status(404).send("Genre not found");
-  }
-
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
 
   if (error) {
     return res.status(400).send(error.message);
   }
 
-  genre.name = req.body.name;
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+
   res.send(genre);
 });
 
