4a91d7e08cb9f1199b0a24da2d871287b857b28a
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-26 18:35:39 +0530

    customers model moved to separate folder

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,26 +1,7 @@
 const express = require("express");
-const Joi = require("joi");
-const mongoose = require("mongoose");
+const { Customer, validate } = require("../models/customer");
 
 const router = express.Router();
-
-const customerSchema = new mongoose.Schema({
-  isGold: Boolean,
-  name: String,
-  phone: String,
-});
-
-const Customer = mongoose.model("Customer", customerSchema);
-
-function validateCustomer(customer) {
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).required(),
-    isGold: Joi.string().alphanum().min(3).required(),
-    phone: Joi.string().alphanum().min(3).required(),
-  });
-
-  return schema.validate(customer);
-}
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find({}).sort("name");
@@ -38,7 +19,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   console.log("req.body", req.body);
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
 
   if (error) {
     return res.status(400).send(error.message);
@@ -61,7 +42,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
 
   if (error) {
     return res.status(400).send(error.message);
