582e9ac0028c4fcfb625e0bc256b5f3307226ad6
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-28 20:42:57 +0530

    added object embedding method

---
+++ vidly/embedding.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log("Error to MongoDB...", err);
+  });
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String,
+});
+
+const Author = mongoose.model("Author", authorSchema);
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: authorSchema ,
+  })
+);
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log("result", result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log("result", result);
+}
+
+async function listCourses() {
+  const courses = await Course.find()
+    .populate("author", "name bio -_id")
+    .select("name");
+  console.log("courses", courses);
+}
+
+
+createCourse('Node js Course', new Author({name: 'Vigneshwaran'}));
