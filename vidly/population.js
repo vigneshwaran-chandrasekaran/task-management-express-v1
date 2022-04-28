db6598785ef30025bd5ab674872ea09c41eb2fc4
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-04-28 20:30:17 +0530

    create author, create couse with author done

--- vidly/population.js
+++ vidly/population.js
@@ -23,5 +23,40 @@ const Course = mongoose.model(
   "Course",
   new mongoose.Schema({
     name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Author'
+    }
   })
 );
+
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log("result", result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log("result", result);
+}
+
+async function listCourses() {
+  const courses = await Course.find().select("name");
+  console.log("courses", courses);
+}
+
+
+// createAuthor('Vigneshwaran', 'Tamilnadu', 'vigneshwaran.top');
+
+createCourse('Node js Course', '626aaa5ff3952e50ce9fd0cb');
