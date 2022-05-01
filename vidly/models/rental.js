b96313f01d61b1cc448c586a1fee836a4956b0ce
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2022-05-01 20:41:41 +0530

    basic rental model file added

---
+++ vidly/models/rental.js
@@ -0,0 +1,71 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const rentalSchema = new mongoose.Schema({
+  title: String,
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: false,
+        minlength: 5,
+        maxlength: 50,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  }, 
+});
+
+const Rental = mongoose.model("Rental", rentalSchema);
+
+function validateRental(rental) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+
+  return schema.validate(rental);
+}
+
+module.exports.Rental = Rental;
+module.exports.validate = validateRental;
